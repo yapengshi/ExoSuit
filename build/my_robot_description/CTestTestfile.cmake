ee093b516289e7f0e6e3176b57bc1c7698886a13
root <m0kiets.ivan@gmail.com>
2025-12-11 06:54:53 +0100

    upd model biped_s40

--- build/my_robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/Synthetic_data_collection/isaac_lab_data_collection/src/my_robot_description
-# Build directory: /root/Synthetic_data_collection/build/my_robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
